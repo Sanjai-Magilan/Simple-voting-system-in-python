a9f1249bb5eddd50c6257e92246e3fcccc31c5e9
SANJAI MAGILAN S <132058187+Sanjai-Magilan@users.noreply.github.com>
2025-08-11 11:29:53 +0530

    Update voting system.py

--- voting system.py
+++ voting system.py
@@ -1,26 +1,19 @@
-#program for simple voting system
+# program for simple voting system
 print("party: zero two,ippo,jojo")
 print("1 for zero two\n2 for ippo\n3 for jojo")
-h=[ ]
-i=[ ]
-e=[ ]
-z=int(input("enter 1 for vote and 0 to exit : "))
-while(z==1):
+h = []
+i = []
+e = []
+z = int(input("enter 1 for vote and 0 to exit : "))
+while ( z == 1 ):
     a = int(input("Enter your choice : "))
     if (a == 1):
-
-            h.append(1)
-
+        h.append(1)
     elif (a == 2):
-
-            i.append(1)
-
-    elif (a == 3):  # how use the while loop perfectly
-
-            e.append(1)
-
+        i.append(1)
+    elif (a == 3): 
+        e.append(1)
     else:
-
             print("Invalid option")
 
 s = (len(h))
@@ -28,31 +21,16 @@ d = (len(i))
 f = (len(e))
 z = int(input("1 for countio 0 for exit : ")) # here is the probleme
 if (s > d and s > f):
-
     print("zero two won")
-
 elif (d > f and d > s):
-
     print("ippo is won")
-
 elif (f > d and f > s):
-
     print("jojo is won")
-
 elif (s == d and f == d and a == 1 and a == 2 and a == 3):
-
     print("All Tie")
-
 elif (s == d and a == 1 and a == 2 and a == 3):
-
     print("zero two and ippo are Tie")
-
 elif (s == f and a == 1 and a == 2 and a == 3):
-
     print("zero two and jojo are tie")
-
 elif (f == d and a == 1 and a == 2 and a == 3):
-
     print("ippo and jojo are tie")
-
-
